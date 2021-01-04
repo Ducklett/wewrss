60efe84591827ec4f59e6b584fd8fbc69e2c041b
Ducklett <ivy16203@gmail.com>
2021-01-04 23:27:40 +0100

    whole lotta shit

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import { AggregateFeed, AggregateFeedType, Channel, Feed, loadChannels, MakeAggregateFeed, MakeFeed, saveChannels } from "./rss"
+import { AggregateFeedType, Channel, Feed, loadChannels, MakeAggregateFeed, MakeFeed, parseRss, RssArticle, saveChannels, updateFeeds } from "./rss"
+import { Article, Button, Div, H2, Img, Main, Small, Summary, A, Details } from './domfluid'
 
 let channels: Channel[] = []
 
@@ -8,17 +9,48 @@ channelList.id = 'channel-list'
 const populateChannelList = (channels: Channel[]): HTMLElement => {
     channelList.innerHTML = null
 
-    const buttons = channels.map((c, i) => {
-        const b = document.createElement('button')
-        b.classList.add('channel-button')
-        b.textContent = c.name
-        b.addEventListener('click', () => console.log(`${i} ${c.name}`))
-        return b
-    })
+    const buttons = channels.map((c, i) =>
+        Button({
+            text: c.name,
+            class: 'channel-button',
+            onClick: () => console.log(`${i} ${c.name}`),
+        })
+    )
 
     for (let b of buttons) channelList.appendChild(b)
 
     return channelList
+}
+
+const showArticles = (articles: RssArticle[]) => {
+    const articleContainer = document.querySelector('#articles')
+    articleContainer.innerHTML = null
+
+    for (let ar of articles) {
+        articleContainer.appendChild(Article({},
+            A({}, H2({ text: ar.title })),
+            Small({ text: ar.date.toDateString() }),
+            Main({ text: ar.description })
+        ))
+    }
+}
+
+const showVideos = (articles: RssArticle[]) => {
+    const articleContainer = document.querySelector('#videos')
+    articleContainer.innerHTML = null
+
+    for (let a of articles) {
+        articleContainer.appendChild(
+            Article({},
+                Div({ onClick: () => fetch(`http://localhost:6969/${a.link}`) },
+                    Img({ src: a.thumbnail }),
+                    H2({ text: a.title })),
+                Small({ text: a.date.toDateString() }),
+                Details({},
+                    Summary({ text: 'Description' }),
+                    Main({ text: a.description }))
+            ))
+    }
 }
 
 const s = document.querySelector('#sidebar')
@@ -31,15 +63,68 @@ document.querySelector('#load-channels').addEventListener('click', () => {
     s.append(populateChannelList(channels))
 })
 
+const CORSproxies = [
+    'https://cors-anywhere.herokuapp.com/',
+    'http://crossorigin.me/',
+    'https://cors-proxy.htmldriven.com/?url=',
+    'http://www.whateverorigin.org/get?url=',
+    'https://cors.io/?',
+    'http://dry-sierra-94326.herokuapp.com/',
+    'https://thingproxy.freeboard.io/fetch/',
+    'https://cors.now.sh/',
+    'https://free-cors-proxy.herokuapp.com',
+    'https://corsproxy.our.buildo.io',
+    'http://www.corsify.me/',
+    'http://gobetween.oklabs.org/pipe/',
+    'http://cors.hyoo.ru/',
+    'https://cors4js.appspot.com/?url=',
+    'http://fuck-cors.com/?url=',
+    'https://proxy-sauce.glitch.me/',
+]
+
 /**** for testing purposes */
 
 channels = [
     MakeFeed('lukesmith.xyz', 'https://lukesmith.xyz/rss.xml'),
     MakeAggregateFeed('Videos', AggregateFeedType.Video, [
-        MakeFeed('Typh', 'https://www.youtube.com/feeds/videos.xml?channel_id=UCdARcqAEmGmU9ppAx8KHiVQ'),
-        MakeFeed('Context free', 'https://www.youtube.com/feeds/videos.xml?channel_id=UCS4FAVeYW_IaZqAbqhlvxlA'),
+        MakeFeed('the art of code', 'https://www.youtube.com/feeds/videos.xml?channel_id=UCcAlTqd9zID6aNX3TzwxJXg'),
+        MakeFeed('meth squad', 'https://www.youtube.com/feeds/videos.xml?channel_id=UC8PqFD4Nqk4PbqvCwudmQww'),
+        MakeFeed('jreg', 'https://www.youtube.com/feeds/videos.xml?channel_id=UCGSGPehp0RWfca-kENgBJ9Q'),
+        MakeFeed('mental outlaw', 'https://www.youtube.com/feeds/videos.xml?channel_id=UCZEvYxVEr0MMUG2ZCpG8bRA'),
+        MakeFeed('luke smith', 'https://www.youtube.com/feeds/videos.xml?channel_id=UC2eYFnH61tmytImy1mTYvhA'),
+        MakeFeed('low level javascript', 'https://www.youtube.com/feeds/videos.xml?channel_id=UC56l7uZA209tlPTVOJiJ8Tw'),
+        MakeFeed('kittydog', 'https://www.youtube.com/feeds/videos.xml?channel_id=UCOdo9GU0enYmMD3shBfDF9g'),
+        MakeFeed('vivzypop', 'https://www.youtube.com/feeds/videos.xml?channel_id=UCLNfsUeQm6zU2eBlQbEy4tw'),
+        MakeFeed('cloy', 'https://www.youtube.com/feeds/videos.xml?channel_id=UCBQAre4rMQmvpKN_-WY5uPQ'),
+        MakeFeed('typh', 'https://www.youtube.com/feeds/videos.xml?channel_id=UCdARcqAEmGmU9ppAx8KHiVQ'),
+        MakeFeed('sr pelo', 'https://www.youtube.com/feeds/videos.xml?channel_id=UCAeDC-kFK69W87jQu7RmxSA'),
+        MakeFeed('context free', 'https://www.youtube.com/feeds/videos.xml?channel_id=UCS4FAVeYW_IaZqAbqhlvxlA'),
+        MakeFeed('hochelaga', 'https://www.youtube.com/feeds/videos.xml?channel_id=UCjP-MiAEn9DPvUHNyGEs7Wg'),
+        MakeFeed('GypsyCrusader', 'https://www.bitchute.com/feeds/rss/channel/Gypsycrusader/')
     ])
 ]
+
+    ; (async () => {
+        const data = await updateFeeds(channels)
+
+        // let l = localStorage.getItem('typh')
+
+        // if (!l) {
+
+        //     const feed = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCdARcqAEmGmU9ppAx8KHiVQ'
+        //     const proxy = CORSproxies[0]
+        //     const url = `${proxy}${feed}`
+        //     console.log(url)
+        //     const res = await fetch(url)
+        //     l = await res.text()
+        //     localStorage.setItem('typh', l)
+        // }
+
+        // const articles = parseRss(l)
+        //     ; (window as any).rss = articles
+        // console.log(articles)
+        showVideos(data.get('Videos'))
+    })()
 
 s.append(populateChannelList(channels))
 
--- rss.ts
+++ rss.ts
@@ -23,6 +23,15 @@ export type SerializedRssData = {
     channels: Channel[],
 }
 
+export type RssArticle = {
+    guid: string,
+    title: string,
+    link: string,
+    date: Date,
+    description: string,
+    thumbnail: string | null,
+}
+
 export const MakeFeed = (name: string, source: string): Feed =>
     ({ kind: 'single', name, source })
 
@@ -35,3 +44,90 @@ export const saveChannels = (channels: Channel[]) =>
 export const loadChannels = (): Channel[] =>
     JSON.parse(localStorage.getItem('rssChannels')) || []
 
+export const parseRss = (feed: string): RssArticle[] => {
+    const dom: Document = new window.DOMParser().parseFromString(feed, "text/xml")
+
+    const isYoutube = dom.querySelector('feed')?.getAttribute('xmlns:yt')
+
+    const items: RssArticle[] = []
+
+    if (isYoutube) {
+
+        const domItems = dom.querySelectorAll('entry')
+
+        for (let it of domItems) {
+            items.push({
+                guid: it.querySelector('link')?.getAttribute('href'),
+                title: it.querySelector('title')?.textContent,
+                // link: it.querySelector('link')?.textContent,
+                link: it.querySelector('link')?.getAttribute('href'),
+                // date: new Date(it.querySelector('pubDate')?.textContent),
+                date: new Date(it.querySelector('published')?.textContent),
+                description: it.querySelector('description')?.textContent,
+                thumbnail: (it.getElementsByTagName('media:group')[0].getElementsByTagName('media:thumbnail')[0]).getAttribute('url'),
+            })
+        }
+    } else {
+        const domItems = dom.querySelectorAll('channel item')
+
+        for (let it of domItems) {
+            items.push({
+                guid: it.querySelector('guid')?.textContent,
+                title: it.querySelector('title')?.textContent,
+                link: it.querySelector('link')?.textContent,
+                date: new Date(it.querySelector('pubDate')?.textContent),
+                description: it.querySelector('description')?.textContent,
+                thumbnail: it.querySelector('enclosure')?.getAttribute('url'),
+            })
+        }
+    }
+
+    console.log(dom)
+
+    return items
+}
+
+export const updateFeed = async (feed: Feed) => {
+    const proxy = 'https://cors-anywhere.herokuapp.com/'
+    const url = `${proxy}${feed.source}`
+    console.log('updating ' + url)
+    const res = await fetch(url)
+    const text = await res.text()
+
+    const articles = parseRss(text)
+
+    return articles
+}
+export const updateFeeds = async (channels: Channel[]) => {
+    const newFeedInfo = new Map<string, RssArticle[]>()
+
+    for (let ch of channels) {
+        let articles: RssArticle[] = []
+
+        switch (ch.kind) {
+            case 'aggregate': {
+
+                for (let fd of ch.feeds) {
+                    const feedArticles = await updateFeed(fd)
+                    articles = articles.concat(feedArticles)
+                }
+
+                articles.sort((a, b) => b.date.getTime() - a.date.getTime())
+                break
+            }
+            case 'single': {
+                articles = await updateFeed(ch)
+                newFeedInfo.set(ch.name, articles)
+                break
+            }
+        }
+
+        newFeedInfo.set(ch.name, articles)
+    }
+
+    localStorage.setItem('feedInfo', JSON.stringify(newFeedInfo))
+
+    console.log('done!')
+    console.log(newFeedInfo)
+    return newFeedInfo
+}
